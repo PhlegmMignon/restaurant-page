d65a78c253d5e3e47765047dfde31315cf072216
Phlegm Mignon <dmah@ucdavis.edu>
2022-07-10 04:28:32 -0700

    Finished header

--- src/index.js
+++ src/index.js
@@ -1,23 +1,85 @@
 import './style.css';
 
 function component() {
-  let content = document.createElement('div');
+  const content = document.createElement('div');
   content.id = 'content';
   
-  let header = document.createElement('div');
-  header.id = 'header';
-  let footer = document.createElement('div');
+
+  const header = document.createElement('div');
+  renderHeader(header, content);
+
+
+
+  
+
+  //Creates main
+  const main = document.createElement('div');
+  main.id = 'main';
+  content.appendChild(main);
+
+
+
+  //Footer stuff
+  const footer = document.createElement('div');
   footer.id = 'footer';
-  content.appendChild(header);
+  footer.innerHTML = 'Copyright © 2022 PhlegmMignon'
   content.appendChild(footer);
 
   
 
-  
-
-
+  //<a href="https://github.com/PhlegmMignon">
+  // <img class="images" src="file:///home/darren/Downloads/GitHub-Mark/PNG/GitHub-Mark-64px.png" alt="">
+  // </a>
 
   return content;
+}
+
+
+function renderHeader(header, content) {
+  header.id = 'header';
+
+  
+  let name = document.createElement('div');
+  name.id = 'name';
+  name.innerHTML = 'Pain Bakery';
+  header.appendChild(name);
+
+  let navbar = document.createElement('div');
+  navbar.id = 'navbar';
+
+  const tab1 = tab('1', 'About us', navbar);
+  const tab2 = tab('2', 'Menu', navbar);
+  const tab3 = tab('3', 'Contact', navbar);
+
+
+  //Add each tab function call here
+  // tab1.addEventListener('click', () => tab1gen());
+  //tab2gen();
+  //tab3gen();
+
+
+
+
+  header.appendChild(navbar);
+  content.appendChild(header);
+
+  return content;
+}
+
+
+const tab = (id, text, navbar) => {
+  let element = document.createElement('div');
+  element.id = 'tab' + id;
+  element.classList.add('tabs');
+  element.innerHTML = text;
+  navbar.appendChild(element);
+  element.addEventListener('click', () => tab1gen());
+  return element;
+}
+
+function tab1gen() {
+  
+
 }
 
 document.body.appendChild(component());
